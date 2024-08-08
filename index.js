56c7b3723cc7cc8d0b3a8a9aec583a2477634815
Mbaduko <nsengiyumvaclement247@gmail.com>
2024-08-08 12:08:17 +0200

    feat: Well done

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const crypto = require('crypto');
+const dns = require('dns');
+const urlHeader = require('url');
+const promisify = require('util').promisify;
 const app = express();
 
 // Basic Configuration
@@ -9,14 +13,53 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({extended:true}))
+
+const urls = {};
+
+const shortenUrl = () => {
+  let  shortId;
+  do {
+    shortId = crypto.randomBytes(2).toString('hex');
+  } while (urls[shortId]);
+  return shortId;
+}
+
+const validUrl = async (link) => {
+  const dnsPromise = promisify(dns.lookup);
+  try {
+    const parsedUrl = new urlHeader.URL(link);
+    const host = parsedUrl.hostname;
+    await dnsPromise(host);
+    return true;
+  } catch (error) {
+    console.log(error)
+    return false;
+  }
+};
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', async (req, res) => {
+  
+  const {url} = req.body;
+  if (!url)
+    return res.json({error:'url Missing'});
+
+  if (! await validUrl(url))
+    return res.json({ error: 'invalid url' });
+
+  const short = shortenUrl ();
+  urls[short] = url;
+  return res.json({ original_url : url, short_url : short});
+});
+
+app.get('/api/shorturl/:short', (req, res) =>{
+  const {short} = req.params;
+  const path = urls[short];
+  res.redirect(path);
 });
 
 app.listen(port, function() {
